367726d206ace50bd8601dacb8d371e98ba01c10
Ashish Kumar <ashishrao9983@gmail.com>
2025-06-21 22:51:55 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import PlayerContextProvider from './context/PlayerContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <BrowserRouter>
-
-     <App />
- 
-   </BrowserRouter>
-    
-
-  </React.StrictMode>,
-);
+      <PlayerContextProvider>
+        <App />
+      </PlayerContextProvider>
+    </BrowserRouter>
+  </StrictMode>,
+)
